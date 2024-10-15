cc6e333e95b244960dc5db3fca2f4ec650c71ae7
sam <samcashlamore@gmail.com>
2024-10-15 10:37:44 -0400

    experimentation with the modrinth api

--- src/http.zig
+++ src/http.zig
@@ -1,3 +1,7 @@
 pub const Status = enum(i32) {
-    ok = 200
+    ok = 200,
+
+    pub fn expect(code: i32, expected: Status) bool {
+        return code == @intFromEnum(expected);
+    }
 };
--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,13 @@
 const std = @import("std");
 const curl = @import("curl");
+
 const http = @import("http.zig");
+const utils = @import("utils.zig");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer if (gpa.deinit() != .ok) @panic("leak");
-    const allocator = gpa.allocator();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
 
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
@@ -14,16 +16,38 @@ pub fn main() !void {
     const easy = try curl.Easy.init(allocator, .{});
     defer easy.deinit();
 
-    const resp = try easy.get("https://staging-api.modrinth.com/");
-    defer resp.deinit();
 
-    if(resp.status_code == @intFromEnum(http.Status.ok)) {
-        try stdout.print("Modrinth response: {s}\n", .{ resp.body.?.items });
+    const args = std.os.argv;
+    if(args.len >= 2) {
+        const first_arg : []const u8 = std.mem.span(args[1]);
+        if(std.mem.eql(u8, first_arg, "modrinth")) {
+            const test_resp = try easy.get("https://api.modrinth.com/");
+            defer test_resp.deinit();
 
-    } else {
-        try stdout.print("Modrinth status: {d}\n", .{ resp.status_code });
+            if(http.Status.expect(test_resp.status_code, .ok)) {
+                if(args.len >= 3) {
+                    const project_arg : []const u8 = std.mem.span(args[2]);
+
+                    const project_url = try std.fmt.allocPrintZ(allocator, "https://api.modrinth.com/v2/project/{s}", .{ project_arg });
+                    defer allocator.free(project_url);
+
+                    const project_resp = try easy.get(project_url);
+                    defer project_resp.deinit();
+
+                    if(http.Status.expect(project_resp.status_code, .ok)) {
+                        try stdout.print("project found!\n{s}\n", .{ project_resp.body.?.items }); 
+                    } else {
+                        try utils.print_err(stdout, "modrinth project {s} not found. error {d}\n", .{ project_arg, project_resp.status_code });
+                    }
+                }
+
+            } else {
+                try utils.print_err(stdout, "modrinth gave invalid status: {d}\n", .{ test_resp.status_code });
+            }
+        } else {
+            try utils.print_err(stdout, "invalid arg {s}\n", .{ first_arg });
+        }
     }
-    
     try bw.flush();
 }
 
